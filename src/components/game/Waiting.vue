e979fffbcf9f6e8b7fbeb093f029d6707a6744b3
Ari Bambang Kurniawan <aribk210@gmail.com>
2021-01-22 08:12:33 +0700

    add gameplay

--- src/components/game/Waiting.vue
+++ src/components/game/Waiting.vue
@@ -1,28 +1,28 @@
-<template>
-  <div class="nes-container is-rounded">
-    <h1>Waiting</h1>
-
-    <i class="nes-octocat animate"></i>
-    <p>Until Master Room Start...</p>
-
-    <button v-if="Number(room.roomMaster) === user.id" type="button" class="nes-btn is-success">
-      Start Game
-    </button>
-  </div>
-</template>
-
 <script>
-export default {
-  name: 'Waiting',
-  computed: {
-    room() {
-      return this.$store.getters.room;
-    },
-    user() {
-      return this.$store.getters.user;
-    },
-  },
-};
+// export default {
+//   name: 'Waiting',
+//   computed: {
+//     room() {
+//       return this.$store.getters.room;
+//     },
+//     user() {
+//       return this.$store.getters.user;
+//     },
+//   },
+//   methods: {
+//     playGame() {
+//       this.$store.dispatch('setPlay');
+//       // const data = {
+//       //   id: Date.now(),
+//       //   round: 0,
+//       //   answered: [],
+//       //   play: true,
+//       // };
+//       // this.$socket.emit('createGame', data);
+//     },
+//   },
+// };
+//
 </script>
 
-<style></style>
+// <style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import usersModule from './modules/users/index';
 import roomsModule from './modules/rooms/index';
+import gamesModule from './modules/games/index';
 
 Vue.use(Vuex);
 
@@ -9,5 +10,6 @@ export default new Vuex.Store({
   modules: {
     users: usersModule,
     rooms: roomsModule,
+    games: gamesModule,
   },
 });
